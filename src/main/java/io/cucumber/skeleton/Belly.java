44c45e6d48a9678b1c38e0575510356c3c09d90d
M.P. Korstanje <rien.korstanje@gmail.com>
2019-11-24 22:48:15 +0100

    Setup and clean up

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,17 @@
 package io.cucumber.skeleton;
 
+import java.util.concurrent.TimeUnit;
+
 public class Belly {
     public void eat(int cukes) {
+
+    }
+
+    public String getStatus() {
+        return null;
+    }
+
+    public void timePasses(int hours, TimeUnit hours1) {
 
     }
 }
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,30 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StepDefinitions {
+    private Belly belly;
+
     @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
-        Belly belly = new Belly();
+    public void I_have_cukes_in_my_belly(int cukes) {
+        belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour(s)")
+    public void I_wait_hours(int hours) {
+        belly.timePasses(hours, HOURS);
+    }
+
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        assertThat(belly.getStatus(), is("Growling!"));
     }
 }
