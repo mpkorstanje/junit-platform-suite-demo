aa2a22c731c75dc8917ccc51dac4857ca4bbf940
M.P. Korstanje <rien.korstanje@gmail.com>
2020-09-19 00:32:16 +0200

    Touch ups

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -3,12 +3,6 @@ package io.cucumber.skeleton;
 import com.github.mpkorstanje.junit.platform.suite.Configuration;
 import com.github.mpkorstanje.junit.platform.suite.SelectClasspathResource;
 import com.github.mpkorstanje.junit.platform.suite.Suite;
-import org.junit.platform.engine.discovery.DiscoverySelectors;
-import org.junit.platform.launcher.EngineFilter;
-import org.junit.platform.launcher.Launcher;
-import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
-import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.suite.api.IncludeEngines;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
@@ -19,17 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Configuration(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
 
-    public static void main(String[] args) {
-        Launcher launcher = LauncherFactory.create();
-
-        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-                .filters(EngineFilter.includeEngines("cucumber"))
-                .selectors(DiscoverySelectors.selectClasspathResource("io/cucumber/skeleton"))
-                .configurationParameter(GLUE_PROPERTY_NAME, "io.cucumber.skeleton")
-                .build();
-
-        launcher.execute(request);
-    }
 }
+
 
 
