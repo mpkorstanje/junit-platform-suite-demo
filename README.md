9cd886a0b46685a0744d4871259ce09334c9c16e
M.P. Korstanje <rien.korstanje@gmail.com>
2020-09-19 00:22:02 +0200

    Touch ups

---
+++ README.md
@@ -0,0 +1,6 @@
+JUnit Platform Suite Demo
+=========================
+
+Usage:
+ * clone https://github.com/mpkorstanje/junit-platform-suite-engine and run `mvn install`
+ * Then run RunCucumberTest   
--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -1,9 +1,13 @@
 package io.cucumber.skeleton;
 
+import com.github.mpkorstanje.junit.platform.suite.Configuration;
 import com.github.mpkorstanje.junit.platform.suite.SelectClasspathResource;
 import com.github.mpkorstanje.junit.platform.suite.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @SelectClasspathResource("io/cucumber/skeleton")
+@Configuration(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
 }
